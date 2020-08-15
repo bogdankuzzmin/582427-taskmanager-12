17a0aa78a30bd0ba920937d81b903c42ff2fcc01
Bogdan <bogdankuzzmin@gmail.com>
2020-08-16 04:02:11 +0600

    added class Sort

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,9 +1,33 @@
-export const createSortTemplate = () => {
+import {createElement} from "../utils.js";
+
+const createSortTemplate = () => {
   return (
     `<div class="board__filter-list">
-    <a href="#" class="board__filter">SORT BY DEFAULT</a>
-    <a href="#" class="board__filter">SORT BY DATE up</a>
-    <a href="#" class="board__filter">SORT BY DATE down</a>
-  </div>`
+      <a href="#" class="board__filter">SORT BY DEFAULT</a>
+      <a href="#" class="board__filter">SORT BY DATE up</a>
+      <a href="#" class="board__filter">SORT BY DATE down</a>
+    </div>`
   );
 };
+
+export default class Sort {
+  constructor() {
+    this._element = null;
+  }
+
+  get template() {
+    return createSortTemplate();
+  }
+
+  get element() {
+    if (!this._element) {
+      this._element = createElement(this.template);
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
