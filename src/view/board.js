cd47292d2a4c299e9828d27abacae2394f80d3ab
Bogdan <bogdankuzzmin@gmail.com>
2020-08-16 04:02:41 +0600

    added class Board

--- src/view/board.js
+++ src/view/board.js
@@ -1,8 +1,29 @@
-export const createBoardTemplate = () => {
-  return (
-    `<section class="board container">
+import {createElement} from "../utils.js";
 
-    <div class="board__tasks"></div>
-    </section>`
+const createBoardTemplate = () => {
+  return (
+    `<section class="board container"></section>`
   );
 };
+
+export default class Board {
+  constructor() {
+    this._element = null;
+  }
+
+  get template() {
+    return createBoardTemplate();
+  }
+
+  get element() {
+    if (!this._element) {
+      this._element = createElement(this.template);
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
