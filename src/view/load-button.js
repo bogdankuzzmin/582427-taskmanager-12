1be15d42fea193f492ca40ac91bf52e9dd9fbadb
Bogdan <bogdankuzzmin@gmail.com>
2020-08-16 04:02:52 +0600

    added class LoadMoreButton

--- src/view/load-button.js
+++ src/view/load-button.js
@@ -1,5 +1,29 @@
-export const createLoadMoreButtonTemplate = () => {
+import {createElement} from "../utils.js";
+
+const createLoadMoreButtonTemplate = () => {
   return (
     `<button class="load-more" type="button">load more</button>`
   );
 };
+
+export default class LoadMoreButton {
+  constructor() {
+    this._element = null;
+  }
+
+  get template() {
+    return createLoadMoreButtonTemplate();
+  }
+
+  get element() {
+    if (!this._element) {
+      this._element = createElement(this.template);
+    }
+
+    return this._element;
+  }
+
+  removeElement() {
+    this._element = null;
+  }
+}
