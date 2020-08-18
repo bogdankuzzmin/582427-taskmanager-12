177b96df0def76064e56dc891c7a2b42bc976c25
Bogdan <bogdankuzzmin@gmail.com>
2020-08-18 14:43:42 +0600

    code refactoring

--- src/const.js
+++ src/const.js
@@ -1,6 +1,6 @@
 export const COLORS = [`black`, `yellow`, `blue`, `green`, `pink`];
 
-export const insertPosition = {
+export const InsertPosition = {
   BEFOREBEGIN: `beforebegin`,
   AFTERBEGIN: `afterbegin`,
   BEFOREEND: `beforeend`,
