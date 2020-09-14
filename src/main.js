9919e92a98e61e0dd018700750239e70926c0142
Bogdan <bogdankuzzmin@gmail.com>
2020-09-14 20:39:41 +0600

    added pattern adapter to convert data from the server

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import {InsertPosition, MenuItem, UpdateType, FilterType} from "./const.js";
 import {render, remove} from "./utils/render.js";
+import Api from "./api.js";
 
 import SiteMenuView from "./view/site-menu.js";
 import StatisticsView from "./view/statistics.js";
@@ -13,9 +14,16 @@ import FilterModel from "./model/filter.js";
 import {generateTask} from "./mock/task.js";
 
 const TASK_COUNT = 22;
+const AUTHORIZATION = `Basic hS2sd3dfSwcl1sa2j`;
+const END_POINT = `https://12.ecmascript.pages.academy/task-manager`;
 
 const tasks = new Array(TASK_COUNT).fill().map(generateTask);
 // console.log(tasks);
+const api = new Api(END_POINT, AUTHORIZATION);
+
+api.getTasks().then((tasks) => {
+  console.log(tasks);
+});
 
 const tasksModel = new TasksModel();
 tasksModel.setTasks(tasks);
@@ -42,10 +50,6 @@ const handleSiteMenuClick = (menuItem) => {
   switch (menuItem) {
     case MenuItem.ADD_NEW_TASK:
       remove(statisticsComponent);
-      // Скрыть статистику
-      // Показать доску
-      // Показать форму добавления новой задачи
-      // Убрать выделение с ADD NEW TASK после сохранения
       boardPresenter.destroy();
       filterModel.setFilter(UpdateType.MAJOR, FilterType.ALL);
       boardPresenter.init();
@@ -54,13 +58,9 @@ const handleSiteMenuClick = (menuItem) => {
       break;
     case MenuItem.TASKS:
       remove(statisticsComponent);
-      // Показать доску
-      // Скрыть статистику
       boardPresenter.init();
       break;
     case MenuItem.STATISTICS:
-      // Скрыть доску
-      // Показать статистику
       boardPresenter.destroy();
       statisticsComponent = new StatisticsView(tasksModel.getTasks());
       render(siteMainElement, statisticsComponent, InsertPosition.BEFOREEND);
